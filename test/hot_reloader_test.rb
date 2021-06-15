5b1eeae782f356ecbfafedfade42cf7bef36a285
Billy.Zheng <vil963@gmail.com>
2021-06-16 01:48:33 +0800

    Add more test.

--- test/hot_reloader_test.rb
+++ test/hot_reloader_test.rb
@@ -10,4 +10,16 @@ class HotReloaderTest < Minitest::Test
     file = File.expand_path("./test_b", __dir__)
     HotReloader.eager_load(file)
   end
+
+  def test_will_listen_zeitwerk_loader
+    loader = Zeitwerk::Loader.new
+    loader.push_dir("#{__dir__}/test_c")
+    HotReloader.will_listen(loader)
+  end
+
+  def test_will_eager_load_zeitwerk_loader
+    loader = Zeitwerk::Loader.new
+    loader.push_dir("#{__dir__}/test_d")
+    HotReloader.eager_load(loader)
+  end
 end
